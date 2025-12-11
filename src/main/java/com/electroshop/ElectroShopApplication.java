6572393170858e9c6a768ded1abbcbc39bbb1ef0
cristalavera <crispipop@gmail.com>
2025-12-11 20:52:08 +0100

    ElectroShop, tarea 2.1: datos, rutas, API y seguridad.

--- src/main/java/com/electroshop/ElectroShopApplication.java
+++ src/main/java/com/electroshop/ElectroShopApplication.java
@@ -1,4 +1,4 @@
-package com.electroshop.pdauf;
+package com.electroshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/electroshop/controller/ElectroShopController.java
+++ src/main/java/com/electroshop/controller/ElectroShopController.java
@@ -1,4 +1,4 @@
-package com.electroshop.pdauf;
+package com.electroshop.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
