20f819659264de4ad0097608af1599a866b34a1b
cristalavera <crispipop@gmail.com>
2025-12-14 13:08:29 +0100

    Actualización Tarea 2.2: implementación con el frontend

--- src/main/java/com/electroshop/controller/ProductoController.java
+++ src/main/java/com/electroshop/controller/ProductoController.java
@@ -13,6 +13,8 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/productos")
+@CrossOrigin(origins = "http://localhost:5173")
+
 public class ProductoController {
 
     @Autowired
